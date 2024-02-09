f88259f85edae69af516b8f6ff16906f6ae10d69
Diego Cardenas <diego.cardenast@gmail.com>
2024-02-09 15:29:55 +0000

    fix: change debug to false and add allowed hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-vli7ewr6_z6w=gr$)4upc7&*w3g-vai!g)@+7+2)22zaipv3h@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-diegocarden-djangoproje-im895p7ehmq.ws-eu108.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-diegocarden-djangoproje-im895p7ehmq.ws-eu108.gitpod.io',
+                '.herokuapp.com']
 
 
 # Application definition
